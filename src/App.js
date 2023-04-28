e8b6daa9afc43794374787d3ff217431fa6cfe44
laiba-shakeel <aqsa.shaqil@gmail.com>
2023-04-28 05:06:34 -0700

    commited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
-
+import { ThemeProvider } from "react-bootstrap";
+import BasicLayout from "./Components/Layouts/BasicLayout";
 function App() {
   return (
- <div>
-  <h1>appplication</h1>
- </div>
+    <ThemeProvider
+    breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+    minBreakpoint="xxs"
+  >
+    <div>
+      <BasicLayout/>
+      </div> 
+
+
+
+
+  </ThemeProvider>
   );
 }
 
