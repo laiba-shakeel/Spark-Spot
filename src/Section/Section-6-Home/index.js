17cdea6aaddecf274ea115bd756cd13406e00392
RabiaSid <rabiasid984@gmail.com>
2023-05-10 21:57:27 +0500

    Section six start

--- src/Section/Section-6-Home/index.js
+++ src/Section/Section-6-Home/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function SectionSixHome() {
   return (
-    <div>SectionSixHome</div>
+  <div className='row container py-2' style={{border:"2px solid red"}}>
+    
+    SectionSixHome
+  </div>
   )
 }
 
