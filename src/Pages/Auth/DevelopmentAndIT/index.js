1bc42b1b8bfae0e9b3ee94fc6480fd917d16b232
laiba-shakeel <aqsa.shaqil@gmail.com>
2023-05-12 20:08:57 +0500

    start section 4 categories

--- src/Pages/Auth/DevelopmentAndIT/index.js
+++ src/Pages/Auth/DevelopmentAndIT/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Layout from "../../../Layouts/BasicLayout/index";
 import SectionOneCatagory from '../../../Section/Section-1-Catagories'
+import SectionFourCatagory from '../../../Section/Section-4-Catagories'
 function DevelopmentAndIt() {
   return (
-  <>
-<Layout>
-  <SectionOneCatagory />
-</Layout>
-  </>
+    <>
+      <Layout>
+        <SectionOneCatagory />
+        <SectionFourCatagory />
+      </Layout>
+    </>
   )
 }
 
